159a1e2a0e72a6b6fc5fa7ba9d8058ddf15ec1c8
michaelacer27 <40066576+michaelacer27@users.noreply.github.com>
2018-06-08 12:23:47 -0400

    Add files via upload

--- michael.py
+++ michael.py
@@ -1,10 +1,1 @@
-import turtle
-my Turtle=turtle.Turtle()
-myTurtle.shape("turtle")
-
-preNum=0
-currNum=1
-nextNum=preNum+currNum
-fibNums=[prenum,currNum]
-myTurtle.color('Blue')
-#Fibonacci Calculation
+print("hellow world")
